e6be5968935bdaf4460d51c18af79b032dd4082b
Kyle .T <45475970+ky-le-t@users.noreply.github.com>
2019-06-22 20:26:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,3 +22,15 @@ client.on('message', msg=>{
         msg.reply('Axie is my name, I am Kyle\'s personal assistant around discord' );
     }
 });
+
+client.on('message', msg=>{
+    if(msg.content ==="spill the beans"){
+        msg.reply('Are you sure?' );
+    }
+});
+
+client.on('message', msg=>{
+    if(msg.content ==="go on Axie"){
+        msg.reply('Kyle likes Tshego' + ' please dont be mad at me');
+    }
+});
